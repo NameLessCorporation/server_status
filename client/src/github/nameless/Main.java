573816837ade0b8123f40bc47d5c8fdab3228af8
Dmitriy <stdian@mail.ru>
2019-06-04 16:05:03 +0800

    Starting making client window

--- client/src/github/nameless/Main.java
+++ client/src/github/nameless/Main.java
@@ -6,7 +6,7 @@ import github.nameless.app.MainWindow;
 public class Main {
 
     public static void main(String[] args) {
-    	new ConnectWindow("NameLess Server Starus - Connect", 520, 230);
-	    //new MainWindow("NameLess Server Status - Client", 900, 600);
+    	//new ConnectWindow("NameLess Server Starus - Connect", 520, 230);
+	    new MainWindow("NameLess Server Status - Client", 900, 600, "192.168.0.208", "stdian");
     }
 }
--- client/src/github/nameless/app/MainWindow.java
+++ client/src/github/nameless/app/MainWindow.java
@@ -3,23 +3,36 @@ package github.nameless.app;
 import javax.swing.*;
 import java.awt.*;
 
+import github.nameless.elements.Button;
+import github.nameless.elements.Label;
+
 public class MainWindow implements Window{
 	private static JFrame frame;
 	private static JPanel panel;
 	String host, user;
 
 	public MainWindow(String name, Integer width, Integer height, String host, String user) {
+		this.host = host;
+		this.user = user;
 		init(name, width, height);
 	}
 
 	@Override
 	public void setLabel() {
+		Label ipLabel = new Label(10, 10, "Server address: " + host);
+		Label userLabel = new Label(10, 30, "You loggined as: " + user);
 
+		panel.add(ipLabel);
+		panel.add(userLabel);
 	}
 
 	@Override
 	public void setButton() {
+		Button stopButton = new Button(8, 50, 125, 30, "Stop server");
+		Button disconnectButton = new Button(8, 80, 125, 30, "Disconnect");
 
+		panel.add(stopButton);
+		panel.add(disconnectButton);
 	}
 
 	@Override
@@ -40,7 +53,9 @@ public class MainWindow implements Window{
 		frame.setPreferredSize(new Dimension(width, height));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setPanel();
-		//Here code
+		setLabel();
+		setField();
+		setButton();
 		frame.add(panel);
 		frame.pack();
 		frame.setResizable(false);
